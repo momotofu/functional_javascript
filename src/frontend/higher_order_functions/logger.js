b536188d7011231cff2900f2bc6dedaf82861efb
Christopher Reece <mrkita.reece@gmail.com>
2019-02-18 21:48:35 +0900

    created basic logger HOF

---
+++ src/frontend/higher_order_functions/logger.js
@@ -0,0 +1,45 @@
+class RemoteLogger {
+  sendMessage(message, value) {
+    console.log(
+      '\nLogging Remote Message -->> ',
+      [new Date().getTime(), message, value])
+  }
+}
+
+const styles = {
+  red: ['color: red', '\x1b[31m'],
+  yellow: ['color: yellow', '\x1b[33m'],
+  blue: ['color: blue', '\m1b[34m'],
+  reset: ['color: unset', '\m1b[0m']
+}
+
+const rl = new RemoteLogger();
+rl.sendMessage('Hello sphere of water and land:', {name: 'Planet Earth'});
+
+
+function logger(styles, useCss = true) {
+  let entry;
+
+  return function configLogger(logger, logMethod = 'log') {
+    const log = logger[logMethod];
+
+    return function loggerColor(color) {
+      const style = styles[color];
+      console.log('style: ', style);
+
+      return function (message, value = null) {
+        // Create entry message (true = browser / false = server)
+        if (useCss)
+          entry = [`%c${message}`, style[0]];
+        else
+          entry = [`${style[1]}${message}${styles['reset'][1]}`];
+
+        log.apply(logger, [...entry, value]);
+
+        return value;
+      }
+    }
+  }
+}
+
+logger(styles, false)(console)('blue')('Try anything', {id: 322});
